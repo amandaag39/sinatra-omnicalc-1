09342a061dc77eb47f0ab9571b7fe99779a4857b
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-01-04 20:48:32 +0000

    Automated tests run and passed

--- app.rb
+++ app.rb
@@ -34,11 +34,36 @@ get("/square_root/results") do
 end
 
 get("/random/new") do
-  "Hello World"
   erb(:random_calc)
 end
 
+get("/random/results") do
+  @min_number = params.fetch("users_min_number").to_f
+  @max_number = params.fetch("users_max_number").to_f
 
+  @random_number = rand(@min_number..@max_number)
+  erb(:random_results)
+end
+
+get("/payment/new") do
+  erb(:payment_calc)
+end
+
+get("/payment/results") do
+  @apr_number = params.fetch("users_apr_number").to_f
+  @years_number = params.fetch("users_years_number").to_i
+  @principal_number = params.fetch("users_principal_number").to_f
+
+  rate_per_period = (@apr_number/100)/12
+  number_of_periods = @years_number * 12
+
+  numerator = rate_per_period * @principal_number
+  denominator= 1 - ((1 + rate_per_period)**-number_of_periods)
+
+  @monthly_payment = (numerator/denominator).to_fs(:currency)
+
+  erb(:payment_results)
+end
 
 get("/") do
   "
