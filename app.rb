75e3c94a2ca72f0bd2b087be9bf30c54aa25bc2a
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-01-04 18:23:24 +0000

    Square and square root calcs finished

--- app.rb
+++ app.rb
@@ -1,6 +1,45 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+
+  erb(:square_root_results)
+end
+
+get("/random/new") do
+  "Hello World"
+  erb(:random_calc)
+end
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
